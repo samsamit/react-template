ff308a66321b0ce8b248180cd956b74d202c39ec
visma-samu-tiainen <samu.tiainen@visma.com>
2024-03-27 21:42:59 +0200

    All setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { useTheme } from "./baseComponents/theme/theme.context";
+import { Badge } from "./baseComponents/ui/badge";
+import { Button } from "./baseComponents/ui/button";
+
 function App() {
-  return (
-    <main className="flex">
-      <h1>React template by samsamit</h1>
-    </main>
-  );
+	const { setTheme } = useTheme();
+	return (
+		<main className="flex bg-background border border-amber-300 w-full h-full">
+			<div className="flex flex-col gap-2">
+				<h1>
+					React template by <Badge>samsamit</Badge>
+				</h1>
+				<Button variant={"default"} onClick={() => setTheme("light")}>
+					Light
+				</Button>
+				<Button variant={"default"} onClick={() => setTheme("dark")}>
+					Dark
+				</Button>
+			</div>
+		</main>
+	);
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { ThemeProvider } from "./baseComponents/theme/theme.provider.tsx";
 import "./main.css";
-import { ThemeProvider } from "./baseComponents/theme.provider.tsx";
 
 if (process.env.NODE_ENV !== "production") {
-  const { default: axe } = await import("@axe-core/react");
-  axe(React, ReactDOM, 1000);
+	const { default: axe } = await import("@axe-core/react");
+	axe(React, ReactDOM, 1000);
 }
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ThemeProvider defaultTheme="dark">
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+const root = document.getElementById("root");
+if (!root) {
+	throw new Error("Root element not found");
+}
+ReactDOM.createRoot(root).render(
+	<React.StrictMode>
+		<ThemeProvider defaultTheme="dark">
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>,
 );
